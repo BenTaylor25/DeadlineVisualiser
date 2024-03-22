3c08180159d3dbed5e8dec660e0937ddef0d9bc5
benlaptop25 <ben.taylor2573@gmail.com>
2024-03-22 20:23:37 +0000

    removed old deadline setup logic

--- deadlines.py
+++ deadlines.py
@@ -1,38 +1,5 @@
-from datetime import date
-
-deadlines = []
-
-try:
-    from private_deadlines import deadlines as p_deadlines
-    deadlines = p_deadlines
-except ModuleNotFoundError:
-    # If `private_deadlines.py` does not exist, use default data.
-    deadlines = [
-        {
-            "label": "test 1",
-            "deadline": date(2024, 3, 10)
-        },
-        {
-            "label": "test 2",
-            "deadline": date(2024, 3, 15)
-        },
-        {
-            "label": "test 3",
-            "deadline": date(2024, 3, 19)
-        },
-        {
-            "label": "test 4",
-            "deadline": date(2024, 3, 22)
-        },
-        {
-            "label": "test 5",
-            "deadline": date(2024, 3, 20)
-        }
-    ]
-
-deadlines.reverse()
-
 from os import walk
+from datetime import date
 import json
 
 DEADLINES_PATH = "./deadline_files"
